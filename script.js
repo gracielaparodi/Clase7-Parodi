bf53f8dff6e8fb9c265eb31a6862962de6ef0f19
Graciela Parodi <gracielaparodi@gmail.com>
2022-09-11 23:52:47 -0300

    first commit

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
  mas de 40 Obesidad grado 3  
 */
 
+
 const masacorporal = ["BAJO DE PESO", "NORMAL", "SOBREPESO", "OBESIDAD GRADO I"];
 const total = masacorporal.push("OBESIDAD GRADO II");
 
@@ -39,7 +40,7 @@ const pacienteencontrado = pacientes.find(pacienteencontrado => pacienteencontra
 console.log(pacienteencontrado);
 
 
-alert("¿Queres saber tu masa corporal?");
+/*alert("¿Queres saber tu masa corporal?");
 
 let peso = parseInt(prompt("¿Cual es tu peso?"));
 let altura = prompt("¿Cual es tu altura?");
@@ -53,6 +54,31 @@ else if (imc <= 25.00 || imc <= 29.99) { alert("SOBREPESO"); }
 else if (imc <= 30.00 || imc <= 34.99) { alert("OBESIDAD GRADO I"); }
 else if (imc <= 35.00 || imc <= 39.99) { alert("OBESIDAD GRADO II"); }
 else if (imc < 40) { alert("OBESIDAD GRADO III"); }
-else { alert("ERROR"); }
+else { alert("ERROR"); }*/
+
+const indice = document.getElementById("Calcular");
+indice.addEventListener("click", function () {
+    calculateIMC();
+});
+
+let pesoInput = document.getElementById("peso");
+let alturaInput = document.getElementById("altura");
+
+peso.setAttribute("placeholder", "Su peso en kilogramos");
+altura.setAttribute("placeholder", "Su altura en metros");
+
+function calculateIMC() {
+    let peso = pesoInput.value;
+    let altura = alturaInput.value;
+    let imc = Math.round(peso / (altura * altura));
+    let mensaje = "";
+    let desc = document.getElementById("desc");
 
 
+    if (imc < 18.5) { alert("BAJO DE PESO | Su imc es " + imc); }
+    else if (imc <= 18.50 || imc <= 24.99) { alert("NORMAL | Su imc es " + imc); }
+    else if (imc <= 25.00 || imc <= 29.99) { alert("SOBREPESO | Su imc es " + imc); }
+    else if (imc <= 30.00 || imc <= 34.99) { alert("OBESIDAD GRADO I " + imc); }
+    else if (imc <= 35.00 || imc <= 39.99) { alert("OBESIDAD GRADO II | Su imc es " + imc); }
+    else if (imc < 40) { alert("OBESIDAD GRADO III | Su imc es " + imc); }
+}
